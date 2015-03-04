c90382868975baeb916cdb287391a5d4ca199dc7
Zach Fedor <fedor@towermarketing.net>
2015-03-04 14:40:18 -0500

    styling top nav

--- wp-content/themes/marketingworks/page-home.php
+++ wp-content/themes/marketingworks/page-home.php
@@ -5,13 +5,18 @@
  *
  */
 
-
-
 // Set the page layout to full-width
-//add_filter( 'genesis_pre_get_option_site_layout', 'mw_home_layout' );
-function mw_home_layout( $opt ) {
+add_filter( 'genesis_pre_get_option_site_layout', 'mw_home_layout' );
+function mw_home_layout( $opt )
+{
     $opt = 'full-width-content'; // You can change this to any Genesis layout
     return $opt;
+}
+
+add_filter( 'genesis_before_content', 'mw_hero_image' );
+function mw_hero_image()
+{
+    echo "fetured image";
 }
 
 genesis();
