2dbbb6430d30c4b04f2a59e7245a2de698f387b7
Zach Fedor <fedor@towermarketing.net>
2015-03-11 17:24:14 -0400

    fixed hard codes in load_assets

--- wp-content/themes/marketingworks/lib/template/load_assets.php
+++ wp-content/themes/marketingworks/lib/template/load_assets.php
@@ -7,7 +7,7 @@ add_action( 'wp_enqueue_scripts', 'gsep_enqueue_scripts', 99);
 function gsep_enqueue_scripts(){
 //    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
 //    wp_enqueue_style('main-css', get_bloginfo('stylesheet_directory') . '/assets/css/theme.min.css');
-    wp_enqueue_style( 'parent-style', get_home_url() . '/wp-content/themes/genesis/style.css' );
+    wp_enqueue_style( 'parent-style', get_home_url() . '/wp-content/themes/Genesis/style.css' );
     wp_enqueue_style('main-css', get_home_url() . '/wp-content/themes/marketingworks/assets/css/theme.min.css');
 
     wp_enqueue_script( 'jquery' );
