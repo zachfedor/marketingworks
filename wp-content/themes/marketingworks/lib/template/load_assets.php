eac235c75193b0a2e4f8f673c051aa67565f5b5f
Zach Fedor <fedor@towermarketing.net>
2015-03-11 17:23:04 -0400

    fixed hard codes in load_assets

--- wp-content/themes/marketingworks/lib/template/load_assets.php
+++ wp-content/themes/marketingworks/lib/template/load_assets.php
@@ -7,8 +7,8 @@ add_action( 'wp_enqueue_scripts', 'gsep_enqueue_scripts', 99);
 function gsep_enqueue_scripts(){
 //    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
 //    wp_enqueue_style('main-css', get_bloginfo('stylesheet_directory') . '/assets/css/theme.min.css');
-    wp_enqueue_style( 'parent-style', 'http://192.168.100.60:3000/marketingworks/wp-content/themes/genesis/style.css' );
-    wp_enqueue_style('main-css', 'http://192.168.100.60:3000/marketingworks/wp-content/themes/marketingworks/assets/css/theme.min.css');
+    wp_enqueue_style( 'parent-style', get_home_url() . '/wp-content/themes/genesis/style.css' );
+    wp_enqueue_style('main-css', get_home_url() . '/wp-content/themes/marketingworks/assets/css/theme.min.css');
 
     wp_enqueue_script( 'jquery' );
     wp_enqueue_script('easytabs-js', get_bloginfo('stylesheet_directory') . '/assets/js/jquery.easytabs.min.js', array('jquery'), '1', true);
